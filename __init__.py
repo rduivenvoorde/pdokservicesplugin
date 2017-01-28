2760921fc36f51a512d6e52699f90e0e41453786
Richard Duivenvoorde <richard@duif.net>
2017-01-28 11:42:00 +0100

    v0.13

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Plugin to easily load the available dutch PDOK (Publieke Dienstverlening Op de Kaart) services."
 def version():
-    return "Version 0.12"
+    return "Version 0.13"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
