cdd18af140fa59227598dbb0c8896cd84c6ebbee
laurent.rothuizen <laurent.rothuizen@kadaster.nl>
2023-10-30 10:53:58 +0100

    Improve modify-layers script

--- pdokservicesplugin/pdokservicesplugin.py
+++ pdokservicesplugin/pdokservicesplugin.py
@@ -373,25 +373,14 @@ class PdokServicesPlugin(object):
             "OGC API - Tiles": "Vector Tiles",
         }
         layername_key = f"{layername_key_mapping[stype]}"
-        # Go to endpoint of ogcapi features since no features in ngr
-        if stype == "OGC API - Features":
-            dataset_metadata_dd = self.get_dd(
-                stype,
-                f'<a title="Bekijk dataset metadata van OAPIF" href="{url}/collections/{layername}">{title} - {layername}</a>',
-            )
-            service_metadata_dd = self.get_dd(
-                stype,
-                f'<a title="Bekijk service metadata van OAPIF" href="{url}">{service_title}</a>',
-            )
-        else:
-            dataset_metadata_dd = self.get_dd(
-                dataset_md_id,
-                f'<a title="Bekijk dataset metadata in NGR" href="https://www.nationaalgeoregister.nl/geonetwork/srv/dut/catalog.search#/metadata/{dataset_md_id}">{dataset_md_id}</a>',
-            )
-            service_metadata_dd = self.get_dd(
-                service_md_id,
-                f'<a title="Bekijk service metadata in NGR" href="https://www.nationaalgeoregister.nl/geonetwork/srv/dut/catalog.search#/metadata/{service_md_id}">{service_md_id}</a>',
-            )
+        dataset_metadata_dd = self.get_dd(
+            dataset_md_id,
+            f'<a title="Bekijk dataset metadata in NGR" href="https://www.nationaalgeoregister.nl/geonetwork/srv/dut/catalog.search#/metadata/{dataset_md_id}">{dataset_md_id}</a>',
+        )
+        service_metadata_dd = self.get_dd(
+            service_md_id,
+            f'<a title="Bekijk service metadata in NGR" href="https://www.nationaalgeoregister.nl/geonetwork/srv/dut/catalog.search#/metadata/{service_md_id}">{service_md_id}</a>',
+        )
         fav_string = ""
         fav_title = ""
         if fav:
